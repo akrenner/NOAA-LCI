65556c9b414ef07fc529890340e9fd92efb96243
akrenner <martin.renner@noaa.gov>
2020-06-03 09:31:49 -0800

    added some stray files -- needs cleanup

---
+++ aws.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+## start aws ec2 instant (type from argument?)
+## install software: R, packages using pacman?
+## copy cache and script files
+## run script
+## rsync results bck
+## shut down aws
+
