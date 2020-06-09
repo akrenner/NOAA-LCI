cbef3aac650eecfd69e70e5cf14cdcdd554ad4cc
akrenner <martin.renner@noaa.gov>
2020-06-09 15:01:57 -0800

    First working parts of CTDwall.R. Can plot sections (transects). Still need to do data cleaning on station names. Still need to work on plotting a station over time (using same approach).

--- CTDwall.R
+++ CTDwall.R
@@ -6,38 +6,109 @@
 ## start with file from dataSetup.R
 rm (list = ls()); load ("~/tmp/LCI_noaa/cache/CTD.RData")  # contains physOc -- raw CTD profiles
 
-
 # rm (list = ls()); load ("~/tmp/LCI_noaa/cache/dataSetupEnd.RData") ## this contains poSS -- CTD summaries
 ## link physOc and stn
 ## should be poSS and stn -- check!
 
 ## set-up plot and paper size
+## bathymetry and coastline
+# tD  <- tempdir()
+# bF <- download.topo (west=-154, east=-150, south=58.5, north=60.3
+#                    , resolution =1
+#                      , destdir = tD
+#                          )
+# bathy <- read.topo (bF)
+# unlink (tD)
+# rm (tD, bF)
 
-
-## plot CTD-profiles along transect
+### data prep
 require ("oce")
-physOc$transDate <- factor (with (physOc, paste (Date, Transect, sep = "-")))
+## define sections
+physOc$DateISO <- format (physOc$isoTime, "%Y-%m-%d")
+physOc$transDate <- factor (with (physOc, paste (DateISO, Transect, sep = " T-")))
+## combine CTD and station meta-data
+physOc <- subset (physOc, !is.na (physOc$Transect))
+## stn <- stn [,which (names (stn) %in%
+##                     c("Line", "Station", "Match_Name", "Lon_decDegree", "Lat_decDegree", "Depth_m")
+##                     )]
+physOc <- cbind (physOc, stn [match (physOc$Match_Name, stn$Match_Name)
+                            , which (names (stn) %in% c("Line", "Lon_decDegree", "Lat_decDegree", "Depth_m"))])
+physOc$Match_Name <- as.factor (physOc$Match_Name)
+# print (summary (physOc))
 
-for (i in length (levels (physOc$transDate))){
-# xC <- subset (physOc, stn$Line == )
-
-## find ctd data and arrange them into sections
-
-
-   # "section" -- that's what I want there
-    define section -- see section class http://127.0.0.1:16810/library/oce/html/section-class.html
-    plot (ctdSection)
-
-    oceColorsTemperature(n)
-    oce.colorsSalinity
-    oce.colorsPAR
-
+# pdf ("~/tmp/LCI-noaa/media/ctdWall.pdf")
+# pdf ("~/Documents//tmp/media/ctdWall.pdf")
+pdf ("ctdWallLines.pdf")
+## define and plot sections
+cat ("Sections to process: ", length (levels (physOc$transDate)))
+for (i in 1:length (levels (physOc$transDate))){
+# for (i in 1:6){
+#    pdf (paste0 ("CTDwall-", levels (physOc$transDate)[i], ".pdf"))
+    xC <- subset (physOc, transDate == levels (physOc$transDate)[i])
+    if (length (levels (as.factor (xC$Match_Name))) > 1){
+        xC <- xC [order (xC$isoTime, xC$Pressure..Strain.Gauge..db.),]
+        ## arrange ctd data into sections
+        ## define section -- see section class http://127.0.0.1:16810/library/oce/html/section-class.html
+        xC <- with (xC # subset (physOc, transDate == levels (physOc$transDate)[i])
+                  , as.section (salinity = Salinity_PSU
+                              , temperature = Temperature_ITS90_DegC
+                              , pressure = Pressure..Strain.Gauge..db.
+                              , longitude = Lon_decDegree
+                              , latitude = Lat_decDegree
+                              , station = Match_Name
+                              , sectionId = transDate
+                                ))
+        plot (xC
+            , which = c (1,2,3,99) # temp, sal, sigmaTheta, map
+            , coastline = "best"      # replace with local
+            , showBottom = "polygon"  # better: provide a "topo" object
+                                        # showBottom = bathy
+                                        #        , drawPalette = TRUE
+           ,  axes = TRUE
+           , showStations = TRUE
+                                        #        , ztype = 'image'
+                                        #        , zcol = oceColorsTemperature # yellow to purple
+                                        #         , zcol = oceColorsSalinity   # yellow-green to blue
+                                        #        , zcol = oceColorsDensity       # dark-purple to light-blue
+                                        # , col = oceColorsTemperature (5)  ## default = horrible jet colors
+###          , mar = c()
+             )
+        mtext (levels (physOc$transDate)[i], side = 3, outer = FALSE, col = "blue")
+    }
+    if (i %% 10 == 0){
+        cat (i, " ")
+        if (i %% 100 == 0) cat ("\n")
+    }
 }
-
+cat ("\n")
+dev.off()
+rm (xC, i)
 
 
 ## plot CTD-profiles of station over time
+pdf ("CTDtime.pdf")
+# for (i in 1:length (levels (physOc$Match_Name))){
+for (i in 1:6){
+    ## section over time? or wrap by hand?
+    xC <- subset (physOc, Match_Name == levels (physOc$Match_Name)[i])
+    if (length (levels (as.factor (xC$Date))) > 1){
+        xC <- with (xC, as.section (salinity = Salinity_PSU
+                               , temperature = Temperature_ITS90_DegC
+                               , pressure = Pressure..Strain.Gauge..db.
+                               , longitude = Lon_decDegree
+                               , latitude = Lat_decDegree
+                               , station = paste0 (Match_Name, DateISO)
+                               , sectionId = transDate
+                                 ))
+        sG <- sectionGrid (xC, p = 'levitus')
 
-
+        ## plot (xC                        # subscript out of bound
+        ##     , xtype = "time"
+        ##     , coastline = "best"
+        ##       )
+    }
+ #   plot (xC, xtype = "time")
+}
+dev.off()
 
 # EOF
