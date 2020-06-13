3542bd1aad74741b4743ffd7661378713db81041
akrenner <martin.renner@noaa.gov>
2020-06-12 16:34:36 -0800

    Layout of CDTwall optimized for letter-sized paper. Script to test that stations numbers are in correct order in master list (they are).

--- CTDwall.R
+++ CTDwall.R
@@ -17,8 +17,9 @@ rm (list = ls()); load ("~/tmp/LCI_noaa/cache/CTD.RData")  # contains physOc -- 
 require ("oce")
 ## define sections
 physOc$DateISO <- format (physOc$isoTime, "%Y-%m-%d")
-physOc$transDate <- factor (with (physOc, paste (DateISO, Transect, sep = " T-")))
+# physOc$transDate <- factor (with (physOc, paste (DateISO, Transect, sep = " T-")))
 physOc$transDate <- factor (with (physOc, paste0 ("T-", Transect, " ", DateISO)))
+physOc$Transect <- factor (physOc$Transect)
 ## combine CTD and station meta-data
 physOc <- subset (physOc, !is.na (physOc$Transect)) ## who's slipping through the cracks??
 ## stn <- stn [,which (names (stn) %in%
@@ -44,14 +45,14 @@ bathy <- "polygon"
 ## Zimmermann bathymetry
 require ("raster")
 require ("marmap")
-bR <- raster ("~/GISdata/LCI/Cook_bathymetry_grid/ci_bathy_grid/w001001.adf")
-## need to crop, downsample (also on Mac?), and reproject to longlat
-## then turn into topo object
 
 ## reproject?  crop?
 # nGrid <- .... ## define new grid -- the missing link
 if (.Platform$OS.type == "unix"){
 ##  bR <- resample (bR, nGrid)
+  bR <- raster ("~/GISdata/LCI/Cook_bathymetry_grid/ci_bathy_grid/w001001.adf") ## not working in RStudio -- need to use decompressed after all?
+  ## need to crop, downsample (also on Mac?), and reproject to longlat
+  ## then turn into topo object
   bRg <- projectRaster(bR, crs = crs ("+proj=longlat +datum=WGS84 +units=m")) ## need to downsample first
   bRb <- as.bathy (bRg)
   bathy <- as.topo (as.bathy (bRg)); rm (bR, bRg)  # still not right
@@ -62,104 +63,124 @@ if (.Platform$OS.type == "unix"){
   bathy <- as.topo (getNOAA.bathy (-154, -150, 58.5, 60.3, resolution = 1, keep = TRUE)) # too coarse for KBay
 }
 
-
-require ("ocedata")
-#mapPlot (coastlineWorld, type = "l", col = "gray")
+require ("ocedata") # coastlineWorldFine
 
 
 
+poAll <- physOc
 
-pdf ("~/tmp/LCI_noaa/media/ctdWall_spSections.pdf") # doesn't work in Rterm.exe
-## define and plot sections
-cat ("Sections to process: ", length (levels (physOc$transDate)))
-for (i in 1:length (levels (physOc$transDate))){
-  xC <- subset (physOc, transDate == levels (physOc$transDate)[i])
-  if (length (levels (factor (xC$Match_Name))) > 1){ ## shouldn't be necessary -- what's up with Transect = NA??
-    xC <- xC [order (xC$isoTime, xC$Pressure..Strain.Gauge..db.),]
-    ## arrange ctd data into sections
-    ## define section -- see section class http://127.0.0.1:16810/library/oce/html/section-class.html
+for (j in 1:length (levels (poAll$Transect))){
+  physOc <- subset (poAll, Transect == levels (poAll$Transect)[j])
+  physOc$transDate <- factor (physOc$transDate)
 
-    #if (nrow (xC) > 1){ ## better than unreliable test above
+  # pdf ("~/tmp/LCI_noaa/media/ctdWall_spSections%02d.pdf" # doesn't work in Rterm.exe, ok with RStudio
+  #      , height = 11, width = 8.5
+  #      )
+  pdf (paste0 ("~/tmp/LCI_noaa/media/CTDwall_", j, "_T-", levels (poAll$Transect)[j], ".pdf")
+       , height = 11, width = 8.5)
+  # par (mfrow = c(6,2))
+  layout (matrix (1:6, 3))
 
-    xC <- with (xC # subset (physOc, transDate == levels (physOc$transDate)[i])
-                , as.section (salinity = Salinity_PSU
-                              , temperature = Temperature_ITS90_DegC
-                              , pressure = Pressure..Strain.Gauge..db.
-                              , longitude = Lon_decDegree
-                              , latitude = Lat_decDegree
-                              , station = Match_Name
-                              , sectionId = transDate
-                ))
+  ## define and plot sections
+  cat ("Sections to process: ", length (levels (physOc$transDate)), "\n")
+  for (i in 1:length (levels (physOc$transDate))){
+    xC <- subset (physOc, transDate == levels (physOc$transDate)[i])
+    if (length (levels (factor (xC$Match_Name))) > 1){ ## shouldn't be necessary -- what's up with Transect = NA??
+      xC <- xC [order (xC$isoTime, xC$Pressure..Strain.Gauge..db.),]
+      ## arrange ctd data into sections
+      ## define section -- see section class http://127.0.0.1:16810/library/oce/html/section-class.html
 
-    pSec <- function (N, zC, ...){
-      plot (xC, which = N, showBottom = bathy, axes = TRUE, ztype = 'image', zcol = zC
-            , stationTicks = TRUE, ...) # zlim?
+      #if (nrow (xC) > 1){ ## better than unreliable test above
+
+      xC <- with (xC # subset (physOc, transDate == levels (physOc$transDate)[i])
+                  , as.section (salinity = Salinity_PSU
+                                , temperature = Temperature_ITS90_DegC
+                                , pressure = Pressure..Strain.Gauge..db.
+                                , longitude = Lon_decDegree
+                                , latitude = Lat_decDegree
+                                , station = Match_Name
+                                , sectionId = transDate
+                  ))
+
+      pSec <- function (N, zC, ...){
+        plot (xC, which = N
+              #, showBottom = bathy
+              , showBottom = FALSE
+              , axes = TRUE, ztype = 'image', zcol = zC
+              , stationTicks = TRUE, ...) # zlim?
+      }
+      #    par (mfrow = c(2,2))
+      pSec (1, oceColorsTemperature)
+#      mtext (levels (physOc$transDate)[i], side = 3, outer = FALSE, col = "blue")
+      title (main = levels (physOc$transDate)[i], col = "blue")
+      pSec (2, oceColorsSalinity)
+#      title (main = levels (physOc$transDate)[i], col = "blue")
+      pSec (3, oceColorsDensity)
+#      title (main = levels (physOc$transDate)[i], col = "blue")
+      # pSec (99, showStations = TRUE, coastline = "coastlineWorldFine")
+      if (0){
+        plot (xC
+              , which = c (1,2,3,99) # temp, sal, sigmaTheta, map
+              #          , coastline = "best"      # replace with local
+              , coastline = "coastlineWorldFine"
+              , showBottom = "polygon"  # better: provide a "topo" object
+              # showBottom = bathy
+              ,  axes = TRUE
+              , showStations = TRUE
+              ###          , mar = c()
+        )
+      }
+      # mtext (levels (physOc$transDate)[i], side = 3, outer = FALSE, col = "blue")
+      #}
     }
-    # par (mfrow = c(2,2))
-    # pSec (1, oceColorsTemperature)
-    # pSec (2, oceColorsSalinity)
-    # pSec (3, oceColorsDensity)
-    pSec (99, showStations = TRUE, coastline = "coastlineWorldFine")
-    if (0){
-      plot (xC
-            , which = c (1,2,3,99) # temp, sal, sigmaTheta, map
-            #          , coastline = "best"      # replace with local
-            , coastline = "coastlineWorldFine"
-            , showBottom = "polygon"  # better: provide a "topo" object
-            # showBottom = bathy
-            ,  axes = TRUE
-            , showStations = TRUE
-            ###          , mar = c()
-      )
+    if (i %% 5 == 0){
+      cat (i, " ")
+      if (i %% 100 == 0) cat ("\n")
     }
-    mtext (levels (physOc$transDate)[i], side = 3, outer = FALSE, col = "blue")
-    #}
   }
-  if (i %% 5 == 0){
-    cat (i, " ")
-    if (i %% 100 == 0) cat ("\n")
-  }
+  dev.off()
+  cat ("\n")
 }
-cat ("\n")
-dev.off()
+physOc <- poAll
+rm (xC, i, poAll)
 
-rm (xC, i)
+
 
 
 if (0){
-## plot CTD-profiles of station over time
-pdf ("CTDtime.pdf")
-# for (i in 1:length (levels (physOc$Match_Name))){
-for (i in 1:6){
-  ## section over time? or wrap by hand?
-  xCp <- subset (physOc, Match_Name == levels (physOc$Match_Name)[i])
-  if (length (levels (as.factor (xC$Date))) > 1){
-    xC <- with (xCp, as.section (salinity = Salinity_PSU
-                                 , temperature = Temperature_ITS90_DegC
-                                 , pressure = Pressure..Strain.Gauge..db.
-                                 , longitude = Lon_decDegree
-                                 , latitude = Lat_decDegree
-                                 , station = paste0 (Match_Name, DateISO)
-                                 , sectionId = transDate
-    ))
-    ## need to add/supply time,
-    xC@metadata$time <- xCp$isoTime
+  ## plot CTD-profiles of station over time
+  pdf ("CTDtime.pdf")
+  # for (i in 1:length (levels (physOc$Match_Name))){
+  for (i in 1:6){
+    ## section over time? or wrap by hand?
+    xCp <- subset (physOc, Match_Name == levels (physOc$Match_Name)[i])
+    if (length (levels (as.factor (xC$Date))) > 1){
+      xC <- with (xCp, as.section (salinity = Salinity_PSU
+                                   , temperature = Temperature_ITS90_DegC
+                                   , pressure = Pressure..Strain.Gauge..db.
+                                   , longitude = Lon_decDegree
+                                   , latitude = Lat_decDegree
+                                   , station = paste0 (Match_Name, DateISO)
+                                   , sectionId = transDate
+      ))
+      ## need to add/supply time,
+      xC@metadata$time <- xCp$isoTime
 
-    sG <- sectionGrid (xC, p = 'levitus')
+      sG <- sectionGrid (xC, p = 'levitus')
 
-    if (0){
-      plot (xC                        # subscript out of bound
-            , which = "temperature" # = 1, salinity 2, density 3
-            , xtype = "time"
-            , ytype = "depth"
-            ## need to define proper z-matrix! -- initiates correct plot may need sectionGrid, as above?
-            # , coastline = "best"
-      )
+      if (0){
+        plot (xC                        # subscript out of bound
+              , which = "temperature" # = 1, salinity 2, density 3
+              , xtype = "time"
+              , ytype = "depth"
+              ## need to define proper z-matrix! -- initiates correct plot may need sectionGrid, as above?
+              # , coastline = "best"
+        )
+      }
     }
+    #   plot (xC, xtype = "time")
   }
-  #   plot (xC, xtype = "time")
-}
-dev.off()
+  dev.off()
 }
 
 
@@ -170,10 +191,11 @@ library(ocedata) #for the coastlineWorldFine data
 data(coastlineWorldFine)
 
 mp <- function() {
-  mapPlot(coastlineWorldFine, projection=proj4string (bR),
+  mapPlot(coastlineWorldFine, #projection=proj4string (bR),
           longitudelim = c(-154.2, -150.5),
           latitudelim = c(58.5, 60.5), col='grey')
 }
+
 
 pdf ("~/tmp/LCI_noaa/media/studyareaMap.pdf")
 mp()
