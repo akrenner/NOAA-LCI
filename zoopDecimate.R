3ae49547deeaeaf69d08128511eb993e1d6469d4
akrenner <martin.renner@noaa.gov>
2020-12-21 15:25:27 -0900

    Refined heuristic search for best set of zooplankton stations.

--- zoopDecimate.R
+++ zoopDecimate.R
@@ -5,23 +5,28 @@
 ## use zoopCoomunity as a template
 
 
+## source ("P:/My Documents/Documents/myDocs/amyfiles/NOAA-LCI/zoopDecimate.R")
+
 rm (list = ls())
 
-runLocal <- TRUE
-runLocal <- FALSE
-
-if (runLocal){
+## set-up for processing on HPC facility
+tr <- require ("pacman")
+if (!tr){
   install.packages ("pacman")
-  load ("~/tmp/LCI_noaa/cache/dataSetupEnd.RData") # from dataSetup.R -- of interest: zooC and zooCenv
-}else{
-  load ("P:/My Documents/Documents/tmp/LCI_noaa/cache/dataSetupEnd.RData")
+}
+tr <- try (load ("~/tmp/LCI_noaa/cache/dataSetupEnd.RData")) # from dataSetup.R -- of interest: zooC and zooCenv
+if (class (tr) == "try-error"){
+  dir.create ("~/tmp/LCI_noaa/cache/", recursive = TRUE, showWarnings = FALSE)
+  file.copy (from = "P:/My Documents/Documents/tmp/LCI_noaa/cache/dataSetupEnd.RData"
+             , to = "~/tmp/LCI_noaa/cache/dataSetupEnd.RData")
+  load ("~/tmp/LCI_noaa/cache/dataSetupEnd.RData")
 }
 set.seed (9)
 Require ("sp")
 Require ("raster")
 Require ("vegan")
 dir.create ("~/tmp/LCI_noaa/media/2019/", recursive = TRUE, showWarnings = FALSE)
-dir.create ("~/tmp/LCI_noaa/cache/", recursive = TRUE, showWarnings = FALSE)
+# dir.create ("~/tmp/LCI_noaa/cache/", recursive = TRUE, showWarnings = FALSE)
 
 
 
@@ -57,7 +62,7 @@ if (1){
 require ("vegan")
 
 
-if (runLocal){
+if (0){
   ## run nMDS -- not of much use here
   nM <- metaMDS (agZoop, distance = "bray", k = 3, try = 200, trymax = 500)
 
@@ -65,16 +70,14 @@ if (runLocal){
   plot (nM, "sites", type = "n")
   text (nM, "sites")
   dev.off()
-
-  ## cluster analysis
-  Require ("vegan")
-  pdf ("~/tmp/LCI_noaa/media/zoopStationYear-Cluster.pdf")
-  #plot (hclust (dist (agZoop, "manhattan"), method = "ward.D"))
-  plot (hclust (vegdist (agZoop, "bray"), method = "ward.D"))
-  dev.off()
 }
 
-
+## cluster analysis
+Require ("vegan")
+pdf ("~/tmp/LCI_noaa/media/zoopStationYear-Cluster.pdf")
+#plot (hclust (dist (agZoop, "manhattan"), method = "ward.D"))
+plot (hclust (vegdist (agZoop, "bray"), method = "ward.D"))
+dev.off()
 
 
 
@@ -88,13 +91,14 @@ disM <- function (cM){# distance measure
 }
 
 ## measure of diversity/distance/variance
-dM <- function (cM){
+dM <- function (cM, meas = "div"){
   require ("vegan")
   ## ideas: mean species CV as a measure of variance
-#  mean (apply (cM, 2, function (x){sd(x)/mean(x)}))
+  #  mean (apply (cM, 2, function (x){sd(x)/mean(x)}))
+
 
   ## species diversity
-   diversity (rowSums (cM), index = "shannon", MARGIN = 1)  ## late saturation
+  diversity (rowSums (cM), index = "shannon", MARGIN = 1)  ## late saturation
   # diversity (rowSums (cM), index = "simpson", MARGIN = 1) ## early saturation
 
   # species richness
@@ -133,12 +137,83 @@ for (i in 1:(nrow (agZoop)-1)){
 }
 
 
+
 pdf ("~/tmp/LCI_noaa/media/zoopStation-unoptimization.pdf")
 plot(divM~Nsamp, rdS, type = "l", lwd = 2, ylab = "species diversity", xlab = "N stations")
 dev.off()
 write.csv (rdS, file = "~/tmp/LCI_noaa/media/zoopStation-unoptimization.csv", row.names = FALSE)
 save.image ("~/tmp/LCI_noaa/cache/zoopDec.RData")
 
+
+
+# rm (list = ls()); load ("~/tmp/LCI_noaa/cache/zoopDec.RData")
+# zM <- agZoop; nOut <- 50; nIter <- 10000
+
+## alternative heuristic algorithm
+bestYofX <- function (zM, nOut = 40, nIter = 1000#, seed = 8
+                      ){ ## inherintly linear or parallel?
+#  set.seed(seed)
+    ## select random stations
+  rSelect <- sort (sample.int (nrow (zM), nOut, replace = FALSE)) # random selection
+  # nSelect <- (1:nrow (zM))[-rSelect] # all left behind
+  # sM <- zM [rSelect,]
+  ## replace 1 location at random -- keep if improvement
+  mutate <- function (testSelect){ ## mutate and test initial selection
+    sM <- zM [testSelect,]
+    notSelect <- (1:nrow (zM))[-testSelect]
+    startC <- dM (zM [testSelect,])
+
+    mutS <- testSelect
+    mutS [sample.int (nOut,1)] <- notSelect [sample.int(length (notSelect), 1)]
+    if (startC < dM (zM [mutS,])){
+      outS <- mutS
+    }else{
+      outS <- testSelect
+    }
+    outS
+  }
+  ## trace convergence
+  tF <- data.frame (iter = 1:nIter, div = NA)
+  for (i in 1:nIter){   # vectorize somehow? any way to speed up? It's intrinsically serial
+    rSelect <- mutate (rSelect)
+    tF$div [i] <- dM (zM [rSelect,])
+  }
+  # plot (div~iter, tF, type = "l")
+  # list (rSelect, dM (zM [rSelect,]))  # return results: best sequence and diversity of result
+  rSelect
+  ## end after nIter (or convergence?)
+}
+
+parBest <- functin (zM, nOut = 40, nRuns = 1000){
+  ## call bestYofX x times in parallel, return the best result. x = nCPUs
+  #  Require ("future.apply")
+  Require ("parallel"); Require ("vegan")
+  nCPUs <- detectCores (logical = TRUE)-1
+  cl <- makeCluster (nCPUs, type = "PSOCK")
+  clusterSetRNGStream (cl, iseed = 42)
+  clusterExport (cl, c("zM", "dM", "Require", "bestYofX", "nOut"))
+  x <- clusterEvalQ (cl, Require ("vegan")) # suppress output
+  ## set seed in parallel XXX
+  # x <- clusterEvalQ (cl, bestYofX (zM, nOut, nIter = 500))
+  pSet <- parLapply (1:1000, bestYofX (zM, nOut, nIter = 500))
+  bestSet <- which.max (sapply (1:length (pSet), function (i){dM (zM [pSet[[i]],])}))
+  stopCluster (cl)
+  pSet [[bestSet]]
+}
+
+
+## cluster this one:  bestYofX (agZoop, 50, nIter = 10)
+## dM (agZoop)=4; nrow (agZoop)=78
+
+outDF <- data.frame (nSite = 1:(nrow (agZoop)-2), div = NA, sites = NA)
+for (i in 1:nrow (outDF)){
+  oSeq <- parBest (agZoop, nOut = nrow (agZoop)-i, nRuns = 640)
+  rdS$heuDiv [i] <- dM (agZoop [oSeq,])
+  rdS$heuSites [i] <- oSeq
+}
+
+
+save.image ("~/tmp/LCI_noaa/cache/zoopDec.RData")
 
 
 
@@ -153,6 +228,9 @@ dev.off()
 sum (df$opt)/1e6
 sum (subset (df$opt, df$k < 14))/1e6
 rm (n, df)
+
+
+
 
 
 ## exhaustive search -- try whether it can be done (NP-hard?!)
@@ -171,23 +249,19 @@ Require ("vegan")
 nCPUs <- detectCores (logical = TRUE)-1
 if (version$os == "mingw32"){
   ## set up cluster
-#  Require ("doParallel")
+  #  Require ("doParallel")
   cl <- makeCluster (nCPUs, type = "PSOCK")
-#  registerDoParallel (cl)
+  #  registerDoParallel (cl)
   clusterExport (cl, c ("agZoop", "dM", "Require"))
   x <- clusterEvalQ (cl, Require ("vegan")) # suppress output
-
   # clusterEvalQ (cl, library (lm4))
   # system.time (save3 <- parLapply (cl, 1:100, ))
-
-
 }
-
 
 
 ## replace just the selected sample size
 for (i in 1:(nrow (agZoop)-1)){
-# for (i in 1:3){
+  # for (i in 10){
   sT <- Sys.time()
   cbn <- combn (1:nrow (agZoop), nrow (agZoop)-i+1)
   if (version$os == "mingw32"){
@@ -195,7 +269,7 @@ for (i in 1:(nrow (agZoop)-1)){
     clusterExport (cl, c ("cbn"))
     oCbn <- which.max (unlist (parLapply (cl, 1:ncol (cbn), fun = function (j){dM (agZoop [cbn [,j],])})))
     # x <- foreach (j = 1:ncol (cbn)) %dopar% dM (agZoop [cbn [,j],])
-      }else{ # forking on Mac or GNU/Linux
+  }else{ # forking on Mac or GNU/Linux
     oCbn <- which.max (unlist (mclapply (1:ncol (cbn), function (j){dM (agZoop [cbn [,j],])}, mc.cores = nCPU)))
   }
   oVect <- cbn [,oCbn]
