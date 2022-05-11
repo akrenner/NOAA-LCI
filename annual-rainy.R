c8bb5f1d5b34bfd6e60654d365e9a8b66ff077ac
akrenner <66282943+akrenner@users.noreply.github.com>
2022-05-11 14:49:00 -0800

    tweaked rainy and annual-salinity.R::freshness graph to fit better together -- and then cut-out freshness, back to salinity. Also cut-out white-space around "="

--- annual-rainy.R
+++ annual-rainy.R
@@ -1,12 +1,16 @@
 
 ## precipitation -- standardized plot
-rm (list = ls())
+rm (list=ls())
 # setwd("~/myDocs/amyfiles/NOAA-LCI/")
+
+## what to show
+pastYear <- TRUE
+ongoingY <- FALSE
 
 
 maO <- 30  # 7 days certainly not working, 14 days not enough either
 # maO <- 1
-qntl = c(0.9) #, 0.8)
+qntl=c(0.9) #, 0.8)
 currentYear <- as.numeric (format (Sys.Date(), "%Y"))-1
 currentCol <- c("lightblue", "blue", "black")
 # currentCol <- "blue"
@@ -24,20 +28,20 @@ if (SWMP){                                   # use SWMP data or NOAA homer airpo
 
 
 
-# plot (subset (hmr$totprcp, hmr$year < 2005), type = "l")
-# plot (totprcp~datetimestamp, data = hmr, subset = year < 2006, type = "l")
+# plot (subset (hmr$totprcp, hmr$year < 2005), type="l")
+# plot (totprcp~datetimestamp, data=hmr, subset=year < 2006, type="l")
 
 ## QCQA moved to windTrend.R
 
 
 ## aggregate data
-tDay <- prepDF (dat = hmr, varName = "totprcp", maO = maO, qntl = qntl)
+tDay <- prepDF (dat=hmr, varName="totprcp", maO=maO, qntl=qntl)
 
 
 ## annual vs average precip
 cat ("\n##\n##\nCurrent year precipitation compared to long-term mean\n")
 anR <- aggregate (totprcp~year, subset (hmr, year <= currentYear)
-                  , sum, na.rm = TRUE)
+                  , sum, na.rm=TRUE)
 summary (subset (anR, year < currentYear)$totprcp)
 subset (anR, year == currentYear)
 cat ("\n##\n##")
@@ -55,76 +59,76 @@ tDay [,which (names (tDay) == "totprcp"):ncol (tDay)] <-
 
 ## annual total percentiles (for text) -- need to work off hmr to get percentiles of annual sum
 cOffY <- ifelse (SWMP, 2005, 1970)
-# yA <- aggregate (totprcp~year+jday, subset (hmr, year > coffY), mean, na.rm = TRUE) ## should exclude >= currentYear
+# yA <- aggregate (totprcp~year+jday, subset (hmr, year > coffY), mean, na.rm=TRUE) ## should exclude >= currentYear
 # yA2 <- aggregate (totprcp~year
 #                   , subset (yA, year < currentYear) #as.numeric (format (Sys.Date(), "%Y"))) # excludeing >= currentYear
-#                   , mean, na.rm = TRUE)
+#                   , mean, na.rm=TRUE)
 yA2 <- aggregate (totprcp~year, subset (hmr, (year > cOffY)&(year < currentYear))
-                  , FUN = sum, na.rm = TRUE)
-ARq <- quantile(yA2$totprcp, 0.5 + c(-1,1) * qntl [1] /2 , na.rm = TRUE)
+                  , FUN=sum, na.rm=TRUE)
+ARq <- quantile(yA2$totprcp, 0.5 + c(-1,1) * qntl [1] /2 , na.rm=TRUE)
 
 ## violin plot of annua rain
 if (0){
   require ("vioplot")
   yA2 <- aggregate (totprcp~year, subset (hmr, (year > cOffY))
-                    , FUN = sum, na.rm = TRUE)
-  x <- vioplot(yA2$totprcp, ylab = "annual precipitation [mm]")
+                    , FUN=sum, na.rm=TRUE)
+  x <- vioplot(yA2$totprcp, ylab="annual precipitation [mm]")
   cYtotprcp <- yA2$totprcp [which (yA2$year == currentYear)]
-  lines (c(0.9, 1.1), rep (cYtotprcp,2), col = "red")
+  lines (c(0.9, 1.1), rep (cYtotprcp,2), col="red")
   # points (0.5, cYtotprcp
-  #         , col  =  "red", pch = 16)
-  text (1.15, cYtotprcp, currentYear, col = "red")
+  #         , col = "red", pch=16)
+  text (1.15, cYtotprcp, currentYear, col="red")
 }
 rm (aF, yA2, cOffY)
 
 
 ## plot
-# pdf (paste0 ("~/tmp/LCI_noaa/media/StateOfTheBay/sa-precip-", ifelse (SWMP, "LE", "AP"), ".pdf"), width = 9, height = 6)
-png (paste0 ("~/tmp/LCI_noaa/media/StateOfTheBay/sa-precip-", ifelse (SWMP, "LE", "AP"), ".png"), width = 1800, height = 1200, res=300)
-par (mar = c (3,4,2,4)+0.1)
-aPlot (tDay, "totprcp", ylab = "daily precipitation [mm]"
-       , currentCol = currentCol
-       , MA = TRUE
-       , pastYear=FALSE, ongoing=FALSE)
+# pdf (paste0 ("~/tmp/LCI_noaa/media/StateOfTheBay/sa-precip-", ifelse (SWMP, "LE", "AP"), ".pdf"), width=9, height=6)
+png (paste0 ("~/tmp/LCI_noaa/media/StateOfTheBay/sa-precip-", ifelse (SWMP, "LE", "AP"), ".png")
+     , width=1800, height=1200, res=300)
+par (mar=c (3,4,2,4)+0.1)
+aPlot (tDay, "totprcp", ylab="daily precipitation [mm]"
+       , currentCol=currentCol
+       , MA=TRUE
+       , pastYear=pastYear, ongoing=ongoingY)
 if (SWMP){title (main="Precipitation at Homer Spit")}
 ## add inch scale
-iAxis (tDay$totprcp, lab = "daily precipitation [inch]")
+iAxis (tDay$totprcp, lab="daily precipitation [inch]")
 
 ## mark high rain days
 cCex <- 2
-text (tDay$jday, ifelse (tDay$pY_totprcp > 10, tDay$pYMA_totprcp + 0.2, NA), labels = "*", col = "blue", cex = cCex)
+text (tDay$jday, ifelse (tDay$pY_totprcp > 10, tDay$pYMA_totprcp + 0.2, NA), labels="*", col="blue", cex=cCex)
 # require ("png")
 # img <- readPNG ("pictograms/rain-cloud.png")
 # hgt <- 0.5; wdh <- 15
 # tRain <- subset (tDay, pY_totprcp > 10)
-# with (tRain, rasterImage (img, xleft = jday - 6, ybottom = pYMA_totprcp + 0.1
-#              , xright = jday - 6 + wdh, ytop = pYMA_totprcp + 0.1 + hgt))
+# with (tRain, rasterImage (img, xleft=jday - 6, ybottom=pYMA_totprcp + 0.1
+#              , xright=jday - 6 + wdh, ytop=pYMA_totprcp + 0.1 + hgt))
 # rm (hgt, wdh)
 
 ## legend
-bP <- legend ("topleft", bty = "n", legend = "") # to get coordinates
-# bP <- cLegend ("topleft" # x = 140, y = max (tDay$totprcp, na.rm = TRUE) # + 4.2  ## better to use "top" and inset? -- or top on blank, then % shift?
-bP <- cLegend (x = bP$rect$left + 55, y = bP$rect$top
-               , qntl = qntl, title = paste (maO, "day moving average")
-               , title.adj = 0.5, currentYear = currentYear
-               , mRange = c (min (hmr$year), currentYear-1)
-               , cYcol = currentCol
-               , pastYear=FALSE, ongoingYear=FALSE
-)
-#  legend (x = bP$rect$left+2, y = bP$rect$top - 0.95, legend = "day with > 10 mm", pch = "*", col = "blue", bty = "n", pt.cex = cCex)
-legend (x = bP$rect$left+2, y = bP$rect$top-bP$rect$h + 0.3 # use -1.2 when plotting 2 years
-        , legend = "day with > 10 mm", pch = "*", col = "blue", bty = "n", pt.cex = cCex)  # add transparent line to fix alignment
+bP <- legend ("topleft", bty="n", legend="") # to get coordinates
+# bP <- cLegend ("topleft" # x=140, y=max (tDay$totprcp, na.rm=TRUE) # + 4.2  ## better to use "top" and inset? -- or top on blank, then % shift?
+bP <- cLegend (x=bP$rect$left + 55, y=bP$rect$top
+               , qntl=qntl, title=paste (maO, "day moving average")
+               , title.adj=0.5, currentYear=currentYear
+               , mRange=c (min (hmr$year), currentYear-1)
+               , cYcol=currentCol
+               , pastYear=pastYear, ongoingYear=ongoingY)
+#  legend (x=bP$rect$left+2, y=bP$rect$top - 0.95, legend="day with > 10 mm", pch="*", col="blue", bty="n", pt.cex=cCex)
+legend (x=bP$rect$left+2, y=bP$rect$top-bP$rect$h + 0.3 # use -1.2 when plotting 2 years
+        , legend="day with > 10 mm", pch="*", col="blue", bty="n", pt.cex=cCex)  # add transparent line to fix alignment
 
 ## totals
 xAl <- bP$rect$left - 50; yAl <- bP$rect$top + 0.3
-text (xAl, yAl, paste0 (round (sum (tDay$pY_totprcp, na.rm = TRUE)), " mm"), col = "blue", pos = 4)
+text (xAl, yAl, paste0 (round (sum (tDay$pY_totprcp, na.rm=TRUE)), " mm"), col="blue", pos=4)
 text (xAl + 35, yAl
-      , paste0 (round (sum (tDay$totprcp, na.rm = TRUE)), " mm", " ["
+      , paste0 (round (sum (tDay$totprcp, na.rm=TRUE)), " mm", " ["
                 , round (as.numeric (ARq [1])), " : "
                 , round (as.numeric (ARq [2])), " mm "
                 , qntl [1]*100, "%-ile]"
       )
-      , col = "darkgray", pos = 4)
+      , col="darkgray", pos=4)
 box()
 dev.off()
 
@@ -132,9 +136,9 @@ rm (bP, xAl, yAl)
 
 
 ## table of average N vs current year N high-rain days
-hmrD <- aggregate(totprcp~jday+year, hmr, FUN = sum)
+hmrD <- aggregate(totprcp~jday+year, hmr, FUN=sum)
 # hmrD <- addTimehelpers(hmrD)
-rainSum <- nEvents(hmrD, "totprcp", thrht = 10)
+rainSum <- nEvents(hmrD, "totprcp", thrht=10)
 print (rainSum)
 rm (hmrD, rainSum)
 ## end of rain summary
@@ -149,7 +153,7 @@ rm (hmrD, rainSum)
 ##################################################################################################
 
 
-rm (list = ls()); load ("~/tmp/LCI_noaa/cache/metDat.RData") # from windTrend.R
+rm (list=ls()); load ("~/tmp/LCI_noaa/cache/metDat.RData") # from windTrend.R
 # load from NOAA ??
 
 maO <- 31
@@ -160,15 +164,15 @@ currentCol <- c ("pink", "red", "orange")
 source ("annualPlotFct.R") # important to call after defining currentCol!
 
 
-tDay <- prepDF (varName = "rh", dat = hmr, maO = maO, qntl = qntl)
+tDay <- prepDF (varName="rh", dat=hmr, maO=maO, qntl=qntl)
 
-# pdf ("~/tmp/LCI_noaa/media/StateOfTheBay/sa-relativeHumidity.pdf", width = 9, height = 6)
-png ("~/tmp/LCI_noaa/media/StateOfTheBay/sa-relativeHumidity.png", width = 1800, height = 1200, res = 300)
-aPlot (tDay, "rh", ylab = "% relative humidity", currentCol = currentCol, MA = TRUE)
-cLegend ("bottomright", qntl = qntl, title = paste (maO, "day moving average")
-         , title.adj = NULL, currentYear = currentYear
-         , mRange = c(min (hmr$year), currentYear - 1)
-         , cYcol = currentCol)
+# pdf ("~/tmp/LCI_noaa/media/StateOfTheBay/sa-relativeHumidity.pdf", width=9, height=6)
+png ("~/tmp/LCI_noaa/media/StateOfTheBay/sa-relativeHumidity.png", width=1800, height=1200, res=300)
+aPlot (tDay, "rh", ylab="% relative humidity", currentCol=currentCol, MA=TRUE)
+cLegend ("bottomright", qntl=qntl, title=paste (maO, "day moving average")
+         , title.adj=NULL, currentYear=currentYear
+         , mRange=c(min (hmr$year), currentYear - 1)
+         , cYcol=currentCol)
 dev.off()
 
 
@@ -179,15 +183,15 @@ dev.off()
 if (0){
 if (1){
   yL <- levels (factor (hmr$year))
-  pdf ("~/tmp/LCI_noaa/media/precipX.pdf", width = 9, height = 6)
+  pdf ("~/tmp/LCI_noaa/media/precipX.pdf", width=9, height=6)
 
   for (i in 1:length (yL)){
-    tDay <- prepDF (varName = "totprcp", dat = subset (hmr, hmr$year != yL [i])  # but but year in Q
-                    , maO = 31, qntl = c(0.5, 0.75)
+    tDay <- prepDF (varName="totprcp", dat=subset (hmr, hmr$year != yL [i])  # but but year in Q
+                    , maO=31, qntl=c(0.5, 0.75)
     )
     with (tDay, annualPlot (MA_totprcp, maL1_totprcp, maU1_totprcp, pYMA_totprcp, jday
-                            , currentCol = "blue", yCaption = "daily rainfall [mm]"))
-    title (main = paste0 ("long-term (grey) = all years but", yL [i]))
+                            , currentCol="blue", yCaption="daily rainfall [mm]"))
+    title (main=paste0 ("long-term (grey)=all years but", yL [i]))
     box()
   }
   dev.off()
@@ -202,16 +206,16 @@ percL <- tDay$perL1_totprcp
 percU <- tDay$perU1_totprcp
 jday <- tDay$jday
 current <- tDay$pY_totprcp
-yCaption = ""
-currentCol = "red"
-perc2L = NA
-perc2U = NA
+yCaption=""
+currentCol="red"
+perc2L=NA
+perc2U=NA
 
 
 ## test -- prep DF
 varName <- "totprcp"
 dat <- hmr
-sumFct <- function (x){mean (x, na.rm = TRUE)}
+sumFct <- function (x){mean (x, na.rm=TRUE)}
 maO <- 31
 currentYear <- 2019
 qntl <- c(0.75)
--- annual-salinity.R
+++ annual-salinity.R
@@ -1,9 +1,9 @@
 ## current year salinity for state of bay report -- modeled on rainy.R
 ## also plot water temperature, shallow and deep
 
-rm (list = ls()); load ("~/tmp/LCI_noaa/cache/SeldTemp.RData")  ## from SeldoviaTemp.R
+rm (list=ls()); load ("~/tmp/LCI_noaa/cache/SeldTemp.RData")  ## from SeldoviaTemp.R
 maO <- 31  # 7 days certainly not working, 14 days not enough either
-qntl = c(0.9)
+qntl=c(0.9)
 maO <- 31 # moving average window
 pMA <- TRUE
 currentCol <- c ("darkblue", "blue")
@@ -18,64 +18,69 @@ source ("annualPlotFct.R")
 ## QCQA -- already done?
 
 
-# tDayHD <- prepDF ("sal", homerDL, maO = 31, qntl = qntl)
-tDayH <- prepDF (dat = homerS, varName = "sal", qntl = qntl, maO = maO) # still has issue with cYMA_sal and cY_sal
-tDayS <- prepDF (dat = sldviaS, varName = "sal", qntl = qntl, maO = maO)
+# tDayHD <- prepDF ("sal", homerDL, maO=31, qntl=qntl)
+tDayH <- prepDF (dat=homerS, varName="sal", qntl=qntl, maO=maO) # still has issue with cYMA_sal and cY_sal
+tDayS <- prepDF (dat=sldviaS, varName="sal", qntl=qntl, maO=maO)
 
 ## plot
-# pdf (paste0 ("~/tmp/LCI_noaa/media/sa-salinity", maO, "-d.pdf"), width = 9, height = 9)
+# pdf (paste0 ("~/tmp/LCI_noaa/media/sa-salinity", maO, "-d.pdf"), width=9, height=9)
 png (paste0 ("~/tmp/LCI_noaa/media/StateOfTheBay/sa-salinity", maO, "-d.png")
-     , width = 1800, height = 1800, res = 300)
-par (mfrow = c(2,1)
-     , mar = c(3,4,4,2)+0.1
+     , width=1800, height=2400, res=300)
+par (mfrow=c(2,1)
+     , mar=c(3,4,4,2)+0.1
 )
 
-aPlot (df=tDayS, vName="sal", MA = pMA, currentCol = currentCol, ylim = c(24, 31.8)
-       , ylab = "salinity")
-title (main = "Seldovia surface")
+aPlot (df=tDayS, vName="sal", MA=pMA, currentCol=currentCol, ylim=c(24, 31.8)
+       , ylab="salinity")
+title (main="Seldovia surface")
 # box()
-cLegend ("bottomleft", inset = 0.05, currentYear = currentYear
-         , mRange = c(min (homerS$year), currentYear -1)
-         , cYcol = currentCol, qntl = qntl [1]
-         # , sYears = c(currentYear-1, currentYear)
+cLegend ("bottomleft", inset=0.05, currentYear=currentYear
+         , mRange=c(min (homerS$year), currentYear -1)
+         , cYcol=currentCol, qntl=qntl [1]
+         # , sYears=c(currentYear-1, currentYear)
          )
 ## add homer data
-aPlot (tDayH, "sal", MA = pMA, currentCol = currentCol, ylim = c(24, 31.8), ylab = "salinity")
-# aPlot (tDayH, "sal", MA = pMA, currentCol = "lightblue", ylim = c(24.5, 31.8))
-title (main = "Homer surface")
+aPlot (tDayH, "sal", MA=pMA, currentCol=currentCol, ylim=c(24, 31.8), ylab="salinity")
+# aPlot (tDayH, "sal", MA=pMA, currentCol="lightblue", ylim=c(24.5, 31.8))
+title (main="Homer surface")
 # box()
 dev.off()
 
 
 
-## reverse of salinity = freshness   (shows freshwater better than 1/sldviaS$sal)
-homerS$fresh <- 32 - homerS$sal
-sldviaS$fresh <- 32 - sldviaS$sal
 
-tDayH <- prepDF (dat = homerS, varName = "fresh", qntl = qntl, maO = maO)
-tDayS <- prepDF (dat = sldviaS, varName = "fresh", qntl = qntl, maO = maO)
-# print (summary (tDayS)); stop ("no more")
+if (0){   ## reverse of salinity=freshness   (shows freshwater better than 1/sldviaS$sal)
+  homerS$fresh <- 32 - homerS$sal
+  sldviaS$fresh <- 32 - sldviaS$sal
 
-png (paste0 ("~/tmp/LCI_noaa/media/StateOfTheBay/sa-waterFreshness", maO, "-d.png"), width = 1800, height = 1800, res = 300)
-par (mfrow = c(2,1)
-     , mar = c(3,4,4,2)+0.1
-)
+  tDayH <- prepDF (dat=homerS, varName="fresh", qntl=qntl, maO=maO)
+  tDayS <- prepDF (dat=sldviaS, varName="fresh", qntl=qntl, maO=maO)
+  # print (summary (tDayS)); stop ("no more")
 
-aPlot (tDayS, "fresh", MA = pMA, currentCol = currentCol , ylim = c(0,7)
-       , ylab = "freshness")
-title (main = "Seldovia surface")
-# box()
-cLegend ("topleft", inset = 0.05, currentYear = currentYear
-         , mRange = c(min (homerS$year), currentYear -1)
-         , cYcol = currentCol, qntl = qntl [1])
-## add homer data
-aPlot (tDayH, "fresh", MA = pMA, currentCol = currentCol, ylim = c(0,7)
-       , ylab = "freshness")
-# aPlot (tDayH, "fresh", MA = pMA, currentCol = "lightblue", ylim = c(24.5, 31.8))
-title (main = "Homer surface")
-# box()
-dev.off()
+  png (paste0 ("~/tmp/LCI_noaa/media/StateOfTheBay/sa-waterFreshness", maO, "-d.png")
+       , width=1800, height=2400, res=300) # taller? rainy: height=1200
+  par (mfrow=c(2,1)
+       # , mar=c(3,4,4,2)+0.1
+       , mar=c (3,4,2,4)+0.1 # values from rainy
+  )
 
+
+
+  aPlot (tDayS, "fresh", MA=pMA, currentCol=currentCol , ylim=c(0,7)
+         , ylab="freshness")
+  title (main="Seldovia surface")
+  # box()
+  cLegend ("topleft", inset=0.05, currentYear=currentYear
+           , mRange=c(min (homerS$year), currentYear -1)
+           , cYcol=currentCol, qntl=qntl [1])
+  ## add homer data
+  aPlot (tDayH, "fresh", MA=pMA, currentCol=currentCol, ylim=c(0,7)
+         , ylab="freshness")
+  # aPlot (tDayH, "fresh", MA=pMA, currentCol="lightblue", ylim=c(24.5, 31.8))
+  title (main="Homer surface")
+  # box()
+  dev.off()
+}
 
 
 
@@ -87,29 +92,29 @@ currentCol <- c ("navyblue", "aquamarine", "lightblue")
 
 instSite <- c ("sldviaS", "sldvia", "homerS", "homer")
 for (j in 1: length (instSite)){
-  tDay <- prepDF (dat = list (sldviaS, sldvia, homerS, homer)[[j]], varName = "temp" # c ("temp", "tempF")[i]
-                  , qntl = qntl, maO = maO)
+  tDay <- prepDF (dat=list (sldviaS, sldvia, homerS, homer)[[j]], varName="temp" # c ("temp", "tempF")[i]
+                  , qntl=qntl, maO=maO)
   # pdf (paste0 ("~/tmp/LCI_noaa/media/StateOfTheBay/sa-", c ("Temp-SST-Seldovia", "Temp-Deep-Seldovia", "Temp-SST-Homer",
   #                                                           "Temp-Deep-Homer")[j]
-  #              , ".pdf"), width = 9, height = 6)
+  #              , ".pdf"), width=9, height=6)
   png (paste0 ("~/tmp/LCI_noaa/media/StateOfTheBay/sa-", c ("Temp-SST-Seldovia", "Temp-Deep-Seldovia", "Temp-SST-Homer",
                                                             "Temp-Deep-Homer")[j]
-               , ".png"), width = 1800, height = 1200, res = 300)
-  par (mar = c(3,4,2,4))
-  aPlot (tDay, "temp", currentCol = currentCol
-         , ylab = expression('Temperature'~'['*degree~'C'*']')
+               , ".png"), width=1800, height=1200, res=300)
+  par (mar=c(3,4,2,4))
+  aPlot (tDay, "temp", currentCol=currentCol
+         , ylab=expression('Temperature'~'['*degree~'C'*']')
   )
-  title (main = c("Seldovia SST", "Seldovia Harbor bottom water temperature", "Homer SST", "Homer bottom water temperature")[j])
+  title (main=c("Seldovia SST", "Seldovia Harbor bottom water temperature", "Homer SST", "Homer bottom water temperature")[j])
 
-  fAxis(c (0, 15), mT = expression('Temperature '~'['*degree~'F'*']')) # could do better XXX
-  bx <- legend ("bottom", inset = 0.1, bty = "n", legend= "")
-  cLegend ("topleft", inset = 0.01
-  #         cLegend ("topleft", inset = 0.01
-                    , currentYear = currentYear
-           , mRange = c (min (list (sldviaS, sldvia, homerS, homer)[[j]]$year), currentYear -1)
-           , cYcol = currentCol
-           , title = paste (maO, "day moving average")
-           , qntl = qntl
+  fAxis(c (0, 15), mT=expression('Temperature '~'['*degree~'F'*']')) # could do better XXX
+  bx <- legend ("bottom", inset=0.1, bty="n", legend= "")
+  cLegend ("topleft", inset=0.01
+  #         cLegend ("topleft", inset=0.01
+                    , currentYear=currentYear
+           , mRange=c (min (list (sldviaS, sldvia, homerS, homer)[[j]]$year), currentYear -1)
+           , cYcol=currentCol
+           , title=paste (maO, "day moving average")
+           , qntl=qntl
   )
 
   box()
