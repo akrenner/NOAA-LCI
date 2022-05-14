f4e9f13237b0aed8ca05acad3455e85cd293a0af
akrenner <66282943+akrenner@users.noreply.github.com>
2022-05-13 16:41:55 -0800

    comments to snowpack

--- annual-snowpack.R
+++ annual-snowpack.R
@@ -1,7 +1,10 @@
 ## SNOTEL snow data
 
-rm (list = ls())
+## bugs: review max and min snow years! should be based on overall peak.
+## max and min currently may be reversed?
 
+
+# rm (list = ls())
 setwd ("~/myDocs/amyfiles/NOAA-LCI/")
 fetchNew <- TRUE  # get fresh data from server? takes longer
 # fetchNew <- FALSE  # get fresh data from server? takes longer
