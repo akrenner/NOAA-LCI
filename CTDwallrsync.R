e25d81916a29c7a4828e39a01f0c1e9f5af30b08
Martin <mrenner@gmx.com>
2022-07-13 21:32:15 -0800

    Towards cross-platform compatibility of CTDwallrsync.R -- addressing issue #11

--- CTDwallrsync.R
+++ CTDwallrsync.R
@@ -10,26 +10,32 @@
 rm (list=ls())
 rcloneDir <- "/Users/Martin.Renner/Applications/rclone-v1.59.0-windows-amd64/"
 
-wd <- getwd()
-setwd (rcloneDir)
-system2(".\rclone.exe copy ~/tmp/LCI_noaa/media/CTDsections/ remote:GulfWatch/plots/CTDsections/")
-shell ("
-
-       ./rclone sync ~/My\ Documents/tmp/LCI_noaa/media/CTDsections/ remote:GulfWatch/plots/CTDsections/
-
-       ")
+## need to make sure that rclone is correctly installed and configured! 
 
 
-setwd (wd)
+if (.Platform$OS.type == "unix"){
+    ## system ("rclone dedupe")
+    system ("rclone sync ~/tmp/LCI_noaa/media/CTDsections/ remote:GulfWatch/plots/CTDsections/")
+}else{    
+    wd <- getwd()
+    setwd (rcloneDir)
+    ## shell or system2 ??
+    shell ("./rclone sync ~/My\ Documents/tmp/LCI_noaa/media/CTDsections/ remote:GulfWatch/plots/CTDsections/")
+    setwd (wd)
+}
 
 
-## paste this into Git-bash:
-cd Applications/rclone-v1.59.0-windows-amd64/
-./rclone sync ~/My\ Documents/tmp/LCI_noaa/media/CTDsections/ remote:GulfWatch/plots/CTDsections/ -P
+if (0){
 
-## same for state-of-the-bay
-./rclone sync ~/My\ Documents/tmp/LCI_noaa/media/StateOfBay/ remote:GulfWatch/plots/StateOfBay/ -P
+    ## paste this into Git-bash:
+    cd Applications/rclone-v1.59.0-windows-amd64/
+           
+  ./rclone sync ~/My\ Documents/tmp/LCI_noaa/media/CTDsections/ remote:GulfWatch/plots/CTDsections/ -P
 
-
-## on occasion, update rclone:
-./rclone selfupdate
+    ## same for state-of-the-bay
+    ./rclone sync ~/My\ Documents/tmp/LCI_noaa/media/StateOfBay/ remote:GulfWatch/plots/StateOfBay/ -P
+    
+    
+    ## on occasion, update rclone:
+    ./rclone selfupdate
+}
