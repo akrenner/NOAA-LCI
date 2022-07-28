1f847543286a5115eaa3b12522e9cc47f44ec416
akrenner <66282943+akrenner@users.noreply.github.com>
2022-07-27 17:15:31 -0800

    Turn comments to quote, so it can be copied&pasted, but also still syntactically correct (can source).

--- CTDsyncGDwall.R
+++ CTDsyncGDwall.R
@@ -38,14 +38,16 @@ if (.Platform$OS.type == "unix"){
 
 
 
-# if (0){
-#   ## on Windows: paste this into Git-bash:
-#   cd Applications/rclone-v1.59.0-windows-amd64/
-#   ./rclone dedupe remote:GulfWatch/plots --dedupe-mode newest -P
-#   ./rclone sync ~/My\ Documents/tmp/LCI_noaa/media/CTDsections/ remote:GulfWatch/plots/CTDsections/ -P
-#   ## same for state-of-the-bay
-#   ./rclone sync ~/My\ Documents/tmp/LCI_noaa/media/StateOfBay/ remote:GulfWatch/plots/StateOfBay/ -P
-#
-#   ## on occasion, update rclone:
-#   ./rclone selfupdate
-# }
+if (0){
+  ## on Windows: paste this into Git-bash:
+  "
+  cd Applications/rclone-v1.59.0-windows-amd64/
+  ./rclone dedupe remote:GulfWatch/plots --dedupe-mode newest -P
+  ./rclone sync ~/My\ Documents/tmp/LCI_noaa/media/CTDsections/ remote:GulfWatch/plots/CTDsections/ -P
+  ## same for state-of-the-bay
+  ./rclone sync ~/My\ Documents/tmp/LCI_noaa/media/StateOfBay/ remote:GulfWatch/plots/StateOfBay/ -P
+
+  ## on occasion, update rclone:
+  ./rclone selfupdate
+  "
+}
