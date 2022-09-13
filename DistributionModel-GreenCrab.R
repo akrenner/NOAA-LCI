885c1d017ef8adfed42e45048b8984612dce9cbc
akrenner <66282943+akrenner@users.noreply.github.com>
2022-09-12 17:10:18 -0800

    stub for green crab model

---
+++ DistributionModel-GreenCrab.R
@@ -0,0 +1,14 @@
+## European Green Crab, Carcinus maenas
+
+## iNaturalist.org positions
+
+
+## SST Jan and Aug
+
+## salinity
+
+## resource selection function, covering 90 % of observations
+
+## apply RSF to global dataset
+
+##
