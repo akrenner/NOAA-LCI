89aa3ee9e7364c7033435dc4ca2201795a3718e8
Martin <mrenner@gmx.com>
2022-09-14 03:09:40 -0800

    basics of RSF are there -- still need to run and fix it.

--- DistributionModel-GreenCrab.R
+++ DistributionModel-GreenCrab.R
@@ -41,7 +41,7 @@ cF <- paste0 ("~/tmp/LCI_noaa/cache/speciesDistribution/", myspecies, ".RData")
 if (file.exists(cF)){
   load (cF)
 }else{
-  gbif_data <- occ_data(scientificName=myspecies, hasCoordinate=TRUE, limit=20000)
+  gbif_data <- occ_data(scientificName=myspecies, hasCoordinate=TRUE, limit=2000)
   # take a look at the downloaded data:
   gbif_data
   ## cleanup: remove southern hemisphere, zeros, anything West of Azores -25 long
@@ -58,7 +58,7 @@ rm (cF)
 ## https://www.ncei.noaa.gov/products/world-ocean-atlas  (downside: files are 3d = big)
 Require ("oceanexplorer")  # works with stars
 sstwin <- get_NOAA ("temperature", spat_res=1, av_period="winter", cache=TRUE)
-sstsum <- get_NOAA ("temperature", spat_res=1, av_period="summar", cache=TRUE)
+sstsum <- get_NOAA ("temperature", spat_res=1, av_period="summer", cache=TRUE)
 sss <- get_NOAA ("salinity", spat_res=1, av_period="annual", cache=TRUE)  # cache about 12 MB each
 
 ## slice out the surface layer
@@ -74,37 +74,74 @@ tD <- tempdir()
 unzip ("~/GISdata/data/coastline/gshhg-shp-2.3.7.zip"
        , junkpaths = TRUE, exdir = tD)
 Require ("sf")
-coastSF <- read_sf (dsn = tD, layer = "GSHHS_h_L1") ## select f, h, i, l, c
+coastG <- read_sf (dsn = tD, layer = "GSHHS_h_L1") ## select f, h, i, l, c
 ## clip to bounding box: NW Atlantic
-b <- st_bbox (coastSF)
-b[c(1,3)] <- c(-25,50)
+b <- st_bbox (coastG)
+b[c(1,3)] <- c(-25,35)
 b[c(2,4)] <- c(0,89)
 bP <- as (st_as_sfc (b), "Spatial") # get spatial polygon for intersect
-coast <- st_intersects (coastSF, bP)
-
+coast <- st_intersects (coastG, bP)
 unlink (tD, TRUE); rm (tD)
 rm (b, bP)
 # coastSF)
 
-## distance from shoreline and buffer to constrain pseudo-locations
 
 ## generate pseudo-negative observations
+## distance from shoreline and buffer to constrain pseudo-locations
+
+## list of ocean grid-points
+b <- st_bbox (coast)
+rPt <- vect (cbind (lon=runif (n=1e4, b[1], b[3])
+                     , lat=runif (n=1e4, b[2], b[4]))
+               , crs="+proj=longlat")
+
+Require ("geosphere")
+cDist <- geosphere::dist2Line (rPt, coast)
+## subset close to shore
+rPt <- rPt [which (cDist < 20e3),]
+
+## faster to find points within coastal buffer?
+## buffer around coast
+Require ("terra")
+cB <- terra::buffer (coast, 20* 1000)
+
+
+## sample at random
+rPt <- rPt [sample (nrow (gbif)),]
+
 
 
 ## lookup environmental values at location of observation and at pseudo-locations
 ## expand environmental values into coast NAs
-Require ("terra")
-gbif$sstW <- terra::extract (sstwin, gbif)
-gbif$sstS <- terra::extract (sstsum, gbif)
-gbif$sss <- terra::extract (sss, gbif)
+exEnv <- function (pt){
+  Require ("terra")
+  sstW <- terra::extract (sstwin, pt)
+  sstS <- terra::extract (sstsum, pt)
+  crab <- data.frame (
+    Tmin=apply (cbind (sstW, sstS), 2, min)
+    , Tmax=apply (cbind (sstW, sstS), 2, max)
+    , sal=terra::extract (sss, pt)
+  )
+  rm (sstW, sstS)
+  crab
+}
 
-
+crab <- rbind (exEnv (gbif)
+               , exEnv (rPt))
+crab$status <- c (rep (1, nrow (gbif)), rep (0, nrow (rPt)))
 
 ## resource selection function, covering 90 % of observations
 Require ("rsf")
+m1 <- rspf (status~Tmin+Tmax+sal, crab, m=0, B=999)
+m2 <- rspf (status~Tmin+Tmax, crab, m=0, B=999)
+CAIC (m1, m2)
+
+summary(m1)
+plot (m1)
+mep (m1)
+kdepairs (m1)
 
 
 ## apply RSF to global dataset
 
 ## project 50 years of global warming
-
