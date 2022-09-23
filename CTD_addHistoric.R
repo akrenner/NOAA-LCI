4da4441579cd56ff0b5dcd9045361c386979ea1b
akrenner <66282943+akrenner@users.noreply.github.com>
2022-09-23 13:30:12 -0800

    stub of CTD_addHistoric.R

---
+++ CTD_addHistoric.R
@@ -0,0 +1,18 @@
+#!/bin/env Rscript
+
+##################################################################
+##                    add legacy CTD datasets                   ##
+##                                                              ##
+##                  Bob DiCino, ADFG raw data                   ##
+##                                                              ##
+##                 Scott Pegau: various sources                 ##
+##################################################################
+
+# library (bannerCommenter)
+
+## unzip
+## read
+## adjust field names and orders
+## minimal to keep: lat lon, sal, depth, pressure, temp, date, time
+## QAQC
+## add our station names
