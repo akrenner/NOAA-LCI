66869352876fd5710517032c8aaa2b6d5da9cce2
Martin <mrenner@gmx.com>
2023-02-06 12:09:09 -0900

    Database analysis: plot sampling effort.

--- FieldNotesDB.R
+++ FieldNotesDB.R
@@ -1,19 +1,19 @@
 ## query acess DB
 rm (list=ls())
 
-if (.Platform$OS.type == "unix"){
-  require (tidyr)
-  adb <- list()
-  AT <- system ("mdb-tables ~/GISdata/LCI/EVOS_LTM_tables/EVOS_LTM.accdb",intern=TRUE) %>%
-    strsplit(" +") %>%
-    unlist()
-  for (i in 1:length (AT)){
-    adb [[i]] <-
-      system (paste0 ("mdb-export ~/GISdata/LCI/EVOS_LTM_tables/EVOS_LTM.accdb ", AT [i])) %>%
-      read.csv(file=stdin())
-  }
-}else{
-}
+# if (.Platform$OS.type == "unix"){
+#   require (tidyr)
+#   adb <- list()
+#   AT <- system ("mdb-tables ~/GISdata/LCI/EVOS_LTM_tables/EVOS_LTM.accdb",intern=TRUE) %>%
+#     strsplit(" +") %>%
+#     unlist()
+#   for (i in 1:length (AT)){
+#     adb [[i]] <-
+#       system (paste0 ("mdb-export ~/GISdata/LCI/EVOS_LTM_tables/EVOS_LTM.accdb ", AT [i])) %>%
+#       read.csv(file=stdin())
+#   }
+# }else{
+# }
 
 atra <- read.csv("~/GISdata/LCI/EVOS_LTM_tables/manualExport/tblAllTransectsCTD.txt")
 tra <- read.csv("~/GISdata/LCI/EVOS_LTM_tables/manualExport/tblTransectEvent.txt") ## trans
@@ -58,24 +58,42 @@ sam <- subset (sam, year > 2016)
 sType <- c("CTD","NUTRIENTS","OA","PHYTOPLANKTON","ZOOPLANKTON")
 sam <- subset (sam, Type %in% sType)
 
+
+if (0){
+## reshape sam to wide
+samC <- with (sam, data.frame (year=factor (year), month=factor (month)
+                               , Transect=factor (Transect), Type, Depth))
+reshape (samC, idvar=c ("year", "month", "Transect"), timevar="Type", direction="wide")
+reshape (samC, idvar=c ("Transect"), timevar="Type", direction="wide")
+
+
+require ("tidyr")
+# spread (samC, key=Type, value=Depth)
+# samC %>% pivot_wider(names_from=c(month,year,Transect) ,values_from=Depth)
+}
+
+
 sampleDF <- data.frame (expand.grid(month=1:12, year=2015:format (Sys.time(), "%Y")))
-sampleDF <- data.frame (expand.grid(month=1:12, year=2017:2021))  ## manual set range!
+sampleDF <- data.frame (expand.grid(Transect=levels (factor (sam$Transect))
+                                    , month=1:12, year=2017:2021))  ## manual set range!
 sampleDF <- cbind (sampleDF, matrix (0, nrow=nrow (sampleDF)
                                      , ncol=length (sType)
                                      , dimnames = list (1:nrow (sampleDF)
                                                         , sType)))
 
+## populate sampleDF with db records
 for (i in 1:nrow (sampleDF)){
   for (j in 1:length (sType)){
-    sC <- subset (sam, year==sampleDF$year[i]) %>%
+    sC <- subset (sam, Transect==sampleDF$Transect [i]) %>%
+                  subset (year==sampleDF$year[i]) %>%
       subset (month==sampleDF$month[i]) %>%
       subset (Type == sType [j]) %>%
       nrow()
-    sampleDF [i,j+2] <- sC
+    sampleDF [i,j+3] <- sC
   }
 }
-xT <- as.matrix (sampleDF [,3:ncol (sampleDF)])
-rownames (xT) <- apply (sampleDF[,1:2], 1, paste, collapse="-")
+xT <- as.matrix (sampleDF [,4:ncol (sampleDF)])
+rownames (xT) <- apply (sampleDF[,1:3], 1, paste, collapse="-")
 
 
 xAxis <- function (side=3, Cx=1,...){
@@ -91,18 +109,13 @@ yAxis <- function (lab, mx, CA=1, ...){
 }
 
 png ("~/tmp/LCI_noaa/media/sampleTable.png", height=11*100, width=5*100, res=100)
-par (las=1)
+par (las=1, mar=c(1,8,4,1))
 image (t (ifelse (xT>0, 1,0)), axes=FALSE)
 xAxis ()
 yAxis (rownames(xT), xT)
 dev.off()
 
 
-
-axis (3, at=1/length (sType)*1:length (sType)
-      , labels = sType
-      )
-# axis (2, labels = )
 
 
 
