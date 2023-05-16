7dc02dd6a832fb3ba6699df7c2be31f5e36164ce
akrenner <66282943+akrenner@users.noreply.github.com>
2023-05-16 13:57:30 -0800

    Improve ctd_editHexFiles.R -- make it more robust and user friendly.
    Now works with low cast numbers (single digits) (bug fix).
    No longer need to edit ctd_editHexFiles.R for every new survey.
    Manual edits.

--- ctd_editHexFiles.R
+++ ctd_editHexFiles.R
@@ -9,34 +9,41 @@
 rm (list=ls())
 
 ## interactively find folder of new survey
-uneditedD <- "~/GISdata/LCI/CTD-processing/Workspace/ctd-data_2017-ongoing/1_Unedited .hex files/2023/2023-02"
+unedDL <- list.dirs("~/GISdata/LCI/CTD-processing/Workspace/ctd-data_2017-ongoing/1_Unedited .hex files/")
+uneditedD <- unedDL [length (unedDL)-1] ## skip "Troubleshooting"
+# "~/GISdata/LCI/CTD-processing/Workspace/ctd-data_2017-ongoing/1_Unedited .hex files/2023/2023-05"
 
 
-newSurvey <- list.files(uneditedD, pattern=".hex", full.names=TRUE, recursive=TRUE)
+hexF <- list.files(uneditedD, pattern=".hex", full.names=TRUE, recursive=TRUE)
 notes <- list.files(uneditedD, pattern=".csv", full.names=TRUE,recursive=TRUE)
 dir.create(gsub ("1_Unedited .hex files", "tmp_2_edited .hex files", uneditedD)
            , showWarnings=FALSE, recursive=FALSE)
 noteT <- read.csv (notes)
+noteT <- subset (noteT, Type=="CTD")
 
 require ("tidyr")
-for (i in 1:length (newSurvey)){
-  hex <- scan (newSurvey[i], what="character", sep="\n")
+for (i in seq_along(hexF)){
+  hex <- scan (hexF[i], what="character", sep="\n")
 
   ## consistency checks:
   ## hex-header and filename report the same cast #
-  headCast <- hex [grep ("^\\* cast", hex)] %>%
+  headCast <- hex [grep ("\\* cast", hex)] %>%
     substr (start=7, stop=10) %>%
-    trimws()
-  fnCast <- newSurvey [i] %>%
-    substr (start=nchar (newSurvey [i])-6, stop=nchar (newSurvey [i])-4)
+    trimws() %>%
+    as.numeric()
+  # %>%
+  #   formatC (width=3, format="d", flag="0")
+  fnCast <- hexF [i] %>%
+    substr (start=nchar (hexF [i])-6, stop=nchar (hexF [i])-4) %>%
+    as.numeric()
   notR <- noteT [match (headCast, noteT$Cast.),]
   notR$Transect <- ifelse (notR$Transect=="AlongBay", "AB", paste0 ("T", notR$Transect))
 
   ## consistency checks
   ## match by cast number
   ## check by time and date -- stop if off by 5 min or more
-  cat (i, headCast, "\n")
-  if (headCast != fnCast){stop (hex [i], "filename cast# does not match header\n")}else{
+  cat (i, "cast:", headCast, "\n")
+  if (headCast != fnCast){stop (hex [2], "filename cast# does not match header\n")}else{
     rm (fnCast)
   }
   ctdTime <- hex [grep ("^\\* cast", hex)] %>%
@@ -45,14 +52,14 @@ for (i in 1:length (newSurvey)){
   deltaT <- difftime (as.POSIXct(paste (notR$Date_isotxt, notR$Time))
                       , ctdTime, units="min") %>%
     abs()
-  if (deltaT > 5){stop ("Notes and cast times differ by > 5 min in ", newSurvey [i])}
+  if (deltaT > 5){stop ("Notes and cast times differ by > 5 min in ", hexF [i])}
   ## end of checks
 
   ## add geographic coordinates, station match_name, etc. to hex-header
   eHex <- c (hex [1:6]
              , paste0 ("** Cook Inlet LTM ", notR$Transect)
              , paste0 ("** Date: ", notR$Date_isotxt)
-             , paste0 ("** Station:S", sprintf ("%02d", as.numeric (notR$Station)))
+             , paste0 ("** Station:S", notR$Station)  #sprintf ("%02d", as.numeric (notR$Station)))
              , paste0 ("** Start Time: ", notR$Time)
              , paste0 ("** Depth (m): ", notR$Depth)
              , paste0 ("** Latitude:", notR$Lat_dDeg)
@@ -67,7 +74,7 @@ for (i in 1:length (newSurvey)){
               , paste0 ("tmp_2_edited .hex files/"
                         , format (ctdTime, "%Y"), "/"
                         , notR$SurveyName, "/")
-              , newSurvey [i])
+              , hexF [i])
   dir.create(nD, showWarnings=FALSE, recursive=TRUE)
   write.table (eHex, file=paste0 (nD, nFN), append=FALSE, quote=FALSE, row.names=FALSE
                , col.names=FALSE)
