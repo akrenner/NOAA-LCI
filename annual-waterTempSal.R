bbc235d170ed858b8819875abab7ed1bdcb9ff6b
akrenner <66282943+akrenner@users.noreply.github.com>
2024-02-14 10:57:16 -0900

    some ideas to chlorophyll auto-correlation

--- annual-waterTempSal.R
+++ annual-waterTempSal.R
@@ -165,7 +165,7 @@ dev.off()
 rm (waterL, hM, i)
 
 ## for troubleshooting annualPlotFct.R::prepDF
-# save.image("~/tmp/LCI_noaa/cache/annualXtmp.RData")
+save.image("~/tmp/LCI_noaa/cache/annualXtmp.RData")
 # rm (list=ls()); load ("~/tmp/LCI_noaa/cache/annualXtmp.RData"); source("annualPlotFct.R"); dat=homerS; varName="chlfluor"; sumFct=function (x){mean (x, na.rm=FALSE)}
 
 
