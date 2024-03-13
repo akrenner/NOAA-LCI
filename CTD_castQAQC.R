08d745624f4d12272998267d7b28c9dc52f05973
akrenner <66282943+akrenner@users.noreply.github.com>
2024-03-13 11:33:13 -0800

    Add temp and salinity x depth plot to cast plots

--- CTD_castQAQC.R
+++ CTD_castQAQC.R
@@ -98,6 +98,7 @@ plotTS (grepl ("^Along", physOc$Match_Name), "month", fn = "along_month")
 
   dirN <- "~/tmp/LCI_noaa/media/CTDcasts/CTDprofiles/"
 dir.create(dirN, showWarnings=FALSE)
+dir.create(paste0 (dirN, "CTD-ts/"), showWarnings=FALSE)
 ## PDF ("CTDprofiles/ALLcasts.pdf")
 require ("oce")
 cat ("\ncount to: ", length (levels (physOc$File.Name)), " \n")
@@ -157,6 +158,15 @@ plotCTDprof <- function (i){
     }, silent=TRUE)
     mtext (levels (physOc$File.Name)[i],side=3, line=-1.25, outer=TRUE)
     dev.off()
+
+    ## extra set of CTD cast plots to match graphics of IOOS model viewer
+    png (paste0 (dirN, "/CTD-ts/", levels (physOc$File.Name)[i], ".png")
+         , res=200, width=2.75*2*200, height=8.5*200)
+    par (mfrow=c(2,1))
+    oce::plotProfile(ctdF, xtype="temperature", ytype="depth", type="l")
+    oce::plotProfile(ctdF, xtype="salinity", ytype="depth", type="l")
+    dev.off()
+
   }else{
     warning (paste (levels (physOc$File.Name)[i]), " has less than 4 records\n\n")
   }
