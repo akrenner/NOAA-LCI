02487ce892bf77a00d75afd8856862aabb1ac7ae
akrenner <66282943+akrenner@users.noreply.github.com>
2024-08-09 16:36:05 -0800

    Package update and cleanup

--- aws.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-## start aws ec2 instant (type from argument?)
-## install software: R, packages using pacman?
-## copy cache and script files
-## run script
-## rsync results bck
-## shut down aws
-
