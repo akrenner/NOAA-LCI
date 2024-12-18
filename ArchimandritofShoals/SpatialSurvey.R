58cde7c330c36f2ca74a9f18ce19cadf31f1fbf2
akrenner <66282943+akrenner@users.noreply.github.com>
2024-12-18 12:20:24 -0900

    fix: name waypoints in exported GPX file for drop-cam survey

--- ArchimandritofShoals/SpatialSurvey.R
+++ ArchimandritofShoals/SpatialSurvey.R
@@ -1,6 +1,14 @@
 ## given a study area and a starting point, design a pseudo-randomized,
 ## spatially balanced survey with an optimal route.
-## Modeled after Russia ALTE surveys.
+## Modeled after Russia ALTE surveys -- now applied to Archimandritof Shoals
+## for mini-drop-cam survey
+
+
+## missing features to implement:
+## - cut-out land from sa
+## - use SDraw::sdraw (type="BAS")
+## - link Concorde TSP solver for speed (only *nix or cygwin)
+
 
 ## author: Martin Renner
 rm (list=ls())
@@ -15,7 +23,7 @@ sa <- sf::read_sf("~/GISdata/LCI/shapefiles/ArchimandritofShoals/ArchimandritofS
 # %>%
 # st_transform(crs=3467) # alaska albert
 # sa <- st_transform(sa, crs=3467) # alaska albert
-
+wrd <- sf::read_sf ("~/GISdata/data/coastline/gshhg-shp/GSHHS_shp/f/GSHHS_f_L1.shp")
 
 ## non-clustered random samples
 pnts <- st_sample (sa, ns, type="random")  ##
@@ -32,19 +40,29 @@ pnts <- st_sample (sa, ns, type="random")  ##
 ## optimal Traveling Salesperson route
 ## starting point
 
-pt <- rbind (st_coordinates (pnts)[,1:2], c(-151.52321, 59.63387))
+pt <- rbind (st_coordinates (pnts)[,1:2], c(-151.52321, 59.63387)) ## start at my house
 ## transform to UTM or
 ptT <- st_as_sf(as.data.frame (pt), crs=4326, coords=c("X", "Y"))
 
 
-nRep <- 50
+nRep <- 64
 require ("doParallel")
 registerDoParallel (8)
 etsp <- ETSP (ptT %>%
                 st_transform(3467) %>%
                 st_coordinates())
 tspS <- solve_TSP (etsp, method = "arbitrary_insertion", two_opt=TRUE, rep = nRep)
+
+## concorde -- requires cygwin or mac/linux
+# concorde_path("/Users/Martin.Renner/Applications/concorde.exe")
+# tspS <- solve_TSP (etsp, method="concorde", control=list (clo="-v -V"))
 # stopCluster (cl)
+
+plot (etsp, tspS, tour_col="red")
+legend ("topright", bty="n",  legend=paste0 ("tour length: "
+                                   , round (tour_length(tspS)/1e3,1)," km"))
+# print (paste (round (tour_length(tspS)/1e3,1), "km"))
+
 
 ## reorder to start with artificial starting point
 resort <- function (idx, startI=length (idx)){
@@ -58,15 +76,14 @@ resort <- function (idx, startI=length (idx)){
 }
 tsp0 <- resort (as.integer (tspS))
 
-ptT <- ptT [as.integer (tsp0),]
 
-
-plot (etsp, tspS, tour_col="red")
-print (paste (round (tour_length(tspS)/1e3,1), "km"))
 
 # plot (sa, col="blue")
 # points (pnts, col="yellow", pch=4)
 
+ptT <- ptT [as.integer (tsp0),]
+ptT$name <- paste0 ("AS_", seq_len(nrow (ptT)))
+#ptT$name <- paste0 ("AS_", seq_len(nrow (ptT)), " (", tsp0, ")")
 
 ## export to gpx file for GPS
 write_sf (ptT, paste0 ("~/tmp/LCI_noaa/data-products/Archimandritof_N=", ns,".gpx")
