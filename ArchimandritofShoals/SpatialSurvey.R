11eef8dc1286fd31a667f6465aff0564bcb39cce
Martin <mrenner@gmx.com>
2025-06-04 17:23:56 -0800

    code cleanup of SpatialSurvey.R

--- ArchimandritofShoals/SpatialSurvey.R
+++ ArchimandritofShoals/SpatialSurvey.R
@@ -32,45 +32,43 @@ sa <- sf::read_sf("~/GISdata/LCI/shapefiles/ArchimandritofShoals/ArchimandritofS
 # %>%
 # st_transform(crs=3467) # alaska albert
 # sa <- st_transform(sa, crs=3467) # alaska albert
-wrd <- sf::read_sf ("~/GISdata/data/coastline/gshhg-shp/GSHHS_shp/f/GSHHS_f_L1.shp") |>
-  st_make_valid()
-# contour <- terra::rast ("~/GISdata/LCI/bathymetry/Kachemak_Bay_DEM_1239/kachemak_bay_ak.asc") |>
-#   as.contour (levels=0)
+if (0){
+  wrd <- sf::read_sf ("~/GISdata/data/coastline/gshhg-shp/GSHHS_shp/f/GSHHS_f_L1.shp") |>
+    st_make_valid()
+  # contour <- terra::rast ("~/GISdata/LCI/bathymetry/Kachemak_Bay_DEM_1239/kachemak_bay_ak.asc") |>
+  #   as.contour (levels=0)
 
-## clip sa polygon
-# sac <- st_intersection(wrd, sa)  ## error in wk_handle.wk_wkb.... edge 746 has duplicate near loop 1
-
+  ## clip sa polygon
+  # sac <- st_intersection(wrd, sa)  ## error in wk_handle.wk_wkb.... edge 746 has duplicate near loop 1
+}
 
 ## non-clustered random samples
 pnts <- st_sample (sa, ns, type="random")  ##
-# pnts <- st_sample (sa, ns, type="hexagonal")  ##
-# require ("spatstat.random")
-# pnts <- st_sample (sa, ns, type="Thomas", scale=1, mu=1)  ##
-# pnts <- st_sample (sa, ns, type="regular")  ##
 
 ## Spatially balanced Pseudo-random samples
-
-# require ("SDraw")             # use Trent McDonald's BAS sampling. THANK YOU, Trent!!
-## make SDraw samples additive, i.e. non-overlapping. Can use all together.
-# spotMother <- sdraw (sa, n = sum (nSList), type = "BAS")
-
 if (bas){
-require ("spbal") ## in place of SDraw
-pnts <- spbal::BAS(sa, n=ns, minRadius=10)$sample
+  # require ("SDraw")             # use Trent McDonald's BAS sampling. THANK YOU, Trent!!
+  ## make SDraw samples additive, i.e. non-overlapping. Can use all together.
+  # spotMother <- sdraw (sa, n = sum (nSList), type = "BAS")
+
+  require ("spbal") ## in place of SDraw
+  pnts <- spbal::BAS(sa, n=ns, minRadius=10)$sample
 }
 
 
 ## optimal Traveling Salesperson route
 ## starting point
 
-pt <- rbind (st_coordinates (pnts)[,1:2], c(-151.52321, 59.63387)) ## start at my house
-## transform to UTM or
-ptT <- st_as_sf(as.data.frame (pt), crs=4326, coords=c("X", "Y"))
+ptT <- rbind (st_coordinates (pnts)[,1:2], c(-151.52321, 59.63387)) |> ## start at my house
+  as.data.frame() |>
+  st_as_sf (crs=4326, coords=c("X", "Y"))
+rm (pnts)
 
 
 nRep <- 64
 require ("doParallel")
 registerDoParallel (8)
+## transform to UTM and solve TSP problem
 etsp <- ETSP (ptT %>%
                 st_transform(3467) %>%
                 st_coordinates())
@@ -88,9 +86,10 @@ pdf ("~/tmp/LCI_noaa/media/Archimandritof_route.pdf")
 # plot (sa, col="lightblue")
 plot (etsp, tspS, tour_col="red")
 legend ("topright", bty="n",  legend=paste0 ("tour length: "
-                                   , round (tour_length(tspS)/1e3,1)," km"))
+                                             , round (tour_length(tspS)/1e3,1)," km"))
 # print (paste (round (tour_length(tspS)/1e3,1), "km"))
 dev.off()
+rm (etsp)
 
 
 ## reorder to start with artificial starting point
@@ -104,15 +103,16 @@ resort <- function (idx, startI=length (idx)){
   idx
 }
 tsp0 <- resort (as.integer (tspS))
-
-
-
-# plot (sa, col="blue")
-# points (pnts, col="yellow", pch=4)
-
 ptT <- ptT [as.integer (tsp0),]
 ptT$name <- paste0 ("AS_", sprintf ("%03d", seq_len(nrow (ptT))))
-#ptT$name <- paste0 ("AS_", seq_len(nrow (ptT)), " (", tsp0, ")")
+
+## points to route
+# sRte <- lapply (1:nrow (ptT)-1, function (i){
+#   st_cast (ptT[i:i+1,], "LINESTRING")
+# }) |>
+#   st_multilinestring()
+# sRtem <- st_multilinestring(do.call ("rbind", sRte))
+
 
 ## export to gpx file for GPS
 dir.create("~/tmp/LCI_noaa/data-products/Archimandritof/", showWarnings=FALSE, recursive=TRUE)
